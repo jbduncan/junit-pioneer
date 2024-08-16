c5c3603fdf3bee1e649f9e809092b12f9480d757
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2024-08-16 15:16:05 +0100

    WIP: refactoring

--- src/main/java/org/junitpioneer/jupiter/collect/IterableContract.java
+++ src/main/java/org/junitpioneer/jupiter/collect/IterableContract.java
@@ -13,11 +13,18 @@ package org.junitpioneer.jupiter.collect;
 import static java.util.Collections.nCopies;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toUnmodifiableList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junitpioneer.internal.PioneerUtils.genericCartesianProduct;
 import static org.junitpioneer.jupiter.collect.IteratorOperation.HAS_NEXT;
 import static org.junitpioneer.jupiter.collect.IteratorOperation.NEXT;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 import org.junit.jupiter.api.DynamicTest;
@@ -43,7 +50,6 @@ public interface IterableContract<E> {
 
 		// +1 so that exhausted iterators are tested
 		int operationSequenceSize = samples().size() + 1;
-
 		var operations = List.of(HAS_NEXT, NEXT);
 		List<List<IteratorOperation>> allOperationSequences = //
 			genericCartesianProduct(nCopies(operationSequenceSize, operations));
@@ -72,8 +78,56 @@ public interface IterableContract<E> {
 		var features = features();
 
 		// TODO: test what happens when neither KNOWN_ORDER nor UNKNOWN_ORDER are provided.
-		new IteratorOperationSequenceChecker<>(actualIterator, operationSequence, expectedElements,
-			features.contains(IterableFeature.KNOWN_ORDER)).check();
+		var knownOrder = features.contains(IterableFeature.KNOWN_ORDER);
+		var remainingExpectedElements = toMutableList(expectedElements);
+		for (var operation : operationSequence) {
+			switch (operation) {
+				case HAS_NEXT:
+					doHasNextOpAndCheck(actualIterator, remainingExpectedElements);
+					break;
+				case NEXT:
+					doNextOpAndCheck(actualIterator, remainingExpectedElements, knownOrder);
+					break;
+				case REMOVE:
+					throw new UnsupportedOperationException("TODO"); // TODO
+			}
+		}
+	}
+
+	private static <E> List<E> toMutableList(SampleElements<E> samples) {
+		return new ArrayList<>(List.of(samples.e0(), samples.e1(), samples.e2()));
+	}
+
+	private void doHasNextOpAndCheck(Iterator<E> iterator, List<E> remainingExpectedElements) {
+		if (remainingExpectedElements.isEmpty()) {
+			// TODO: add a message parameter
+			assertFalse(iterator.hasNext());
+			return;
+		}
+
+		// TODO: add a message parameter
+		assertTrue(iterator.hasNext());
+	}
+
+	private void doNextOpAndCheck(Iterator<E> iterator, List<E> remainingExpectedElements, boolean knownOrder) {
+		if (remainingExpectedElements.isEmpty()) {
+			// TODO: add a message parameter
+			assertThrows(NoSuchElementException.class, iterator::next);
+			return;
+		}
+
+		E nextValue = iterator.next();
+
+		if (knownOrder) {
+			// TODO: add a message parameter
+			assertEquals(remainingExpectedElements.get(0), nextValue);
+			remainingExpectedElements.remove(0);
+		} else {
+			int index = remainingExpectedElements.indexOf(nextValue);
+			// TODO: add a message parameter
+			assertTrue(index >= 0);
+			remainingExpectedElements.remove(index);
+		}
 	}
 
 }
